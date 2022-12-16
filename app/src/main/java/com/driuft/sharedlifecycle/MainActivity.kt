db666e5bfd9b2ad142480434a63748daf77b4dbe
driuft <air.socarras@gmail.com>
2022-12-16 00:47:41 -0500

    Initial commit

---
+++ app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.driuft.sharedlifecycle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
